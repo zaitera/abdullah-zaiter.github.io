f5f1f1ccf50c0c1d0ee92c5159ecb9c3bd1b4824
Abdullah <abdullah-zaiter@bitbucket.org>
2019-08-05 11:37:49 -0300

    e agora jose ?

---
+++ precache-manifest.d8821bd8314c93abad4252fd2715135d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e93c72972a910093771295540df9f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6847d3987dd049b8e7c9",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "8371ced7ec235016ef4f",
+    "url": "/static/js/2.ab35712b.chunk.js"
+  },
+  {
+    "revision": "6847d3987dd049b8e7c9",
+    "url": "/static/js/main.2916f463.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
